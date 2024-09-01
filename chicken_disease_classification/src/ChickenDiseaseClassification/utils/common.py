bfb71eb6f757753af7b7e114185eb2c2e5cb06c9
Ankita Pillay <ankpillay@gmail.com>
2024-09-01 22:17:05 +0200

    added some files

---
+++ chicken_disease_classification/src/ChickenDiseaseClassification/utils/common.py
@@ -0,0 +1,53 @@
+import os
+from box.exceptions import BoxValueError
+from ChickenDiseaseClassification import logger
+
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import List
+import base64
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """Reads the yaml file and returns      
+    """
+    
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml_file.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+    
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose = True):
+    """Creates list of directories
+
+    Args:
+        path_to_directories (list): _description_
+        verbose (bool, optional): _description_. Defaults to True.
+    """
+    
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok= True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+            
+
+def save_json(path:Path, data:dict):
+    """save the json data
+    """
+    
+    with open(path, "w") as f:
+        json.dump(data, f ,indent = 4)
+        
+    logger.info(f"json file saved at: {path}")
+            
+    
+
